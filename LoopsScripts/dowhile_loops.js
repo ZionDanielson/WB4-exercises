1ef7fdf2cb8325350e702321eb43fcbd4029a645
Zion Danielson <zionsdocs@gmail.com>
2023-05-09 11:54:16 -0400

    for_loops.js

---
+++ LoopsScripts/dowhile_loops.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let phrase = "I love loops";
+let i = 0;
+do {
+ console.log(phrase);
+ i++;
+} while (i < 7)
---
+++ LoopsScripts/for_loops.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let phrase = "I love loops";
+
+for (let i = 0; i < 7; i++) {
+    console.log(phrase);
+   }
+   
